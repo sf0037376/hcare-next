a47a34eb48b803cee69732b24596122942594997
redlilydev <naveen@redlily.com>
2026-04-27 00:36:54 +0530

    rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ["lucide-react"],
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        tailwindcss: "./node_modules/tailwindcss",
-      },
-    },
-  },
 };
 
 export default nextConfig;
